0402571fcc3d5684b03ef9fba00616911ef46182
Eric H <eghove@gmail.com>
2020-05-30 18:51:50 -0500

    initial scaffold

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+//const key = require("./keys");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const bodyParser = require("body-parser");
+
+// pull in API routes
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
