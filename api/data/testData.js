8b0a40dc95d7d55ae6aa690b9079f2d26b11e473
Eric H <eghove@gmail.com>
2020-06-07 09:41:25 -0500

    setting some separate test data

---
+++ api/data/testData.js
@@ -0,0 +1,16 @@
+const testData = {
+  documents: [
+    {
+      id: "1",
+      language: "en",
+      text: "My wife is amazing and I love her!",
+    },
+    {
+      id: "2",
+      language: "en",
+      text: "My cats keep throwing up everwhere and it's terrible!",
+    },
+  ],
+};
+
+module.exports = testData;
--- api/services/sentimentService.js
+++ api/services/sentimentService.js
@@ -1,5 +1,6 @@
 const key = require("../../keys");
 const axios = require("axios");
+const testData = require("../data/testData");
 
 const subscription_key = key.textAnalyticsKey.toString();
 const endpoint = key.textAnalyticsEndpoint.toString();
@@ -11,21 +12,7 @@ module.exports = {
       method: "post",
       headers: { "Ocp-Apim-Subscription-Key": subscription_key },
       url: endpoint + "text/analytics/v2.1/sentiment",
-      data: {
-        documents: [
-          {
-            id: "1",
-            language: "en",
-            text: "My wife is amazing and I love her!",
-          },
-          {
-            id: "2",
-            language: "es",
-            text:
-              "Este ha sido un dia terrible, llegué tarde al trabajo debido a un accidente automobilistico.",
-          },
-        ],
-      },
+      data: testData,
     })
       .then(function (response) {
         res.json(response.data);
